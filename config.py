1726692718646b7bfbe47fc9560aa3ff41710845
arislanbek1019 <108214916+arislanbek1019@users.noreply.github.com>
2026-07-01 22:03:55 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+BOT_TOKEN = os.getenv("8369235380:AAFMvustW9dDdv22EIg_INrZfBI8BcD7uAs")
+GROQ_API_KEY = os.getenv("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")
+
+GROQ_MODEL = "llama-3.3-70b-versatile"
